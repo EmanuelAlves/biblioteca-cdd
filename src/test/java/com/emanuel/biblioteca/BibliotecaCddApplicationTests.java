916e7e4d3884ae9991829ee7e3c3e9a3d9d2c9ad
Emanuel Alves Costa <emanuel.alvesc@gmail.com>
2021-01-25 21:22:30 -0300

    initial commit.

---
+++ src/test/java/com/emanuel/biblioteca/BibliotecaCddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emanuel.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaCddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
